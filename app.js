da26b07e49f768c6498b06be23d0dec4fbd9cf57
Bhakti Jadhav <jadhavbhakti80@gmail.com>
2023-03-28 19:49:10 +0530

    updates in callback url

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/secrets'
+    callbackURL: 'https://sneaky-production.up.railway.app/auth/google/secrets'
   },
   async function (accessToken, refreshToken, profile, done) {
     try {
@@ -85,7 +85,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+    callbackURL: "https://sneaky-production.up.railway.app/auth/facebook/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ facebookId: profile.id }, function (err, user) {
